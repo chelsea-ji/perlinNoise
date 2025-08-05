2370ddedaddef2c13f780c83aaa6f6b440433ebb
Chelsea Ji <chelseaji464@gmail.com>
2025-08-05 08:46:22 -0400

    Update Vector2.java

--- Vector2.java
+++ Vector2.java
@@ -9,38 +9,16 @@ public class Vector2 {
 		this.x = x;
 		this.y = y;
 	}
-	
-	public double[] getComponents() {
-		return new double[] {x, y};
-	}
-	
-	public double getX() {
-		return this.x;
-	}
-	
-	public double getY() {
-		return this.y;
-	}
-	
-	public double getLength() {
-		return Math.sqrt(x * x + y * y);
-	}
-	
+
 	public double dot(Vector2 v) {
 	    return this.x * v.x + this.y * v.y;
 	}
 	
-    public void rotateBy(double angle) {
+  public void rotateBy(double angle) {
         double cos = Math.cos(angle);
         double sin = Math.sin(angle);
         double rx = x * cos - y * sin;
         y = x * sin + y * cos;
         x = rx;
-    }
-    
-    public void normalize() {
-    	double magnitude = getLength();
-        x /= magnitude;
-        y /= magnitude;
-    }
+  }
 }
